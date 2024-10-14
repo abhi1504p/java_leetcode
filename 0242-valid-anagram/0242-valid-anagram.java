910273aacae639e27be8a9584eab9374d8f465d3
Abhishek Prajapati <118985083+abhi1504p@users.noreply.github.com>
2024-10-14 22:40:21 +0530

    Time: 2 ms (97.89%), Space: 44.5 MB (38.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,9 +4,8 @@ class Solution {
         char tempArray1[] = t.toCharArray();
         Arrays.sort(tempArray);
         Arrays.sort(tempArray1);
-        String str = String.valueOf(tempArray);
-        String str1 = String.valueOf(tempArray1);
-        if(str1.equals(str)){
+        
+        if( String.valueOf(tempArray).equals(String.valueOf(tempArray1))){
             return true;
         }
        
