30a7fd53440983eb1cdd685f170cd6a49a7785a4
Abhishek Prajapati <118985083+abhi1504p@users.noreply.github.com>
2024-11-03 08:25:53 +0530

    Time: 0 ms (100%), Space: 41.8 MB (15.4%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -4,26 +4,21 @@ class Solution {
             return false;
 
         }
-        
-       
+
         StringBuilder str = new StringBuilder(s);
-         if (str.equals(goal)) {
+        if (str.equals(goal)) {
             return true;
         }
         char store;
-      for(int i=0;i<s.length();i++){
-        store=s.charAt(i);
-        str.deleteCharAt(0);
-        str.append(store);
-        if(str.toString().equals(goal)){
-            return true;
+        for (int i = 0; i < s.length(); i++) {
+            store = s.charAt(i);
+            str.deleteCharAt(0);
+            str.append(store);
+            if (str.toString().equals(goal)) {
+                return true;
+            }
+
         }
-
-        
-      }
-       
-
-       
 
         return false;
 
